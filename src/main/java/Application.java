f22ff8800800e0ab3592273cc1aed41d4f1a300b
shalitha <mdsrwijethilaka@gmail.com>
2019-03-06 01:54:10 -0800

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,6 @@ public class Application {
 
     public static void main(String[] args){
         System.out.println("simple tg");
-        System.out.println("tesvvvvvvvt");
+        System.out.println("tesvvvt");
     }
 }
