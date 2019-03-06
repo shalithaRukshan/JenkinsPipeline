b71fb4921135abb84ef31889a1f87d616a568a8b
shalitha <mdsrwijethilaka@gmail.com>
2019-03-06 04:29:13 -0800

    tttttttttttttttttttttttttttttt
    y

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,6 @@ public class Application {
 
     public static void main(String[] args){
         System.out.println("simptg");
-        System.out.println("testaaaaaaaaaaaaaadfgfg");
+        System.out.println("tesaaaaaaadfgfg");
     }
 }
