eee6edce926a7a879c9575fae399b326c3b94c64
shalitha <mdsrwijethilaka@gmail.com>
2019-03-05 22:39:02 -0800

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,7 @@
 public class Application {
 
     public static void main(String[] args){
-        System.out.println("simple testing");
+        System.out.println("simple tsting");
         System.out.println("test");
     }
 }
