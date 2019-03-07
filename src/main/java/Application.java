5c9ed0d92a0304baf918fa02b854edb6fc45d14e
Shalitha_08790 <shalitha.wijethilaka@axiatadigitallabs.com>
2019-03-07 17:36:52 +0530

    test

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,59 +1,18 @@
 public class Application {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int target = -5;
-int num = 3;
+        int num = 3;
 
-target =- num;  // Noncompliant; target = -3. Is that really what's meant?
-target =+ num; // Noncompliant; target = 3
+        target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+        target = +num; // Noncompliant; target = 3
         System.out.println("simp            tg");
         System.out.println("taaa  ssss  aaadffg");
-        
-                try {
-            URL mySite = new URL("http://www.cs.utexas.edu/~scottm");
-            URLConnection yc = mySite.openConnection();
-            Scanner in = new Scanner(new InputStreamReader(yc.getInputStream()));
-            int count = 0;
-            while (in.hasNext()) {
-                System.out.println(in.next());
-                count++;
-            }
-            System.out.println("Number of tokens: " + count);
-            in.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-                int[] freqs = createFreqTableURL("http://www.utexas.edu/");
 
-        if( freqs.length == 0)
-            System.out.println("No frequency table created due to problems when reading from file");
-        else{
-            for(int i = 0; i < NUM_ASCII_CHAR; i++){
-                System.out.println("charcater code: " + i + " ,character: " + (char)i + " ,frequency: " + freqs[i]);
-            }
-            System.out.println("Total characters in file: " + sum(freqs));
-        }
+    }
 
-
-        freqs = new int[]{};
-        try{
-            freqs = createTable("ciaFactBook2008.txt");
-        }
-        catch(FileNotFoundException e){
-            System.out.println("File not found. Unable to create freq table" + e);
-        }
-        catch(IOException e){
-            System.out.println("Problem while reading from file. Unable to create freq table" + e);
-        }
-        if( freqs.length == 0)
-            System.out.println("No frequency table created due to problems when reading from file");
-        else{
-            for(int i = 0; i < freqs.length; i++){
-                System.out.println("charcater code: " + i + " ,character: " + (char)i + " ,frequency: " + freqs[i]);
-            }
-            System.out.println("Total characters in file: " + sum(freqs));
-        }
+    public void test(Object obj){
+        MyClass myClass = (MyClass) obj;
     }
 }
 
