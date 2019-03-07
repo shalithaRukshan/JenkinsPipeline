e9dc6a3598bb6202e884d83952420b6c52b48379
shalitha <mdsrwijethilaka@gmail.com>
2019-03-07 14:49:39 +0530

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,6 @@ public class Application {
 
     public static void main(String[] args){
         System.out.println("simp            tg");
-        System.out.println("taaa    aaadffg");
+        System.out.println("taaa  ssss  aaadffg");
     }
 }
