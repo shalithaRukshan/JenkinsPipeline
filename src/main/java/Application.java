276e63b4ad3e784d46b17eae48088cdfffc67baf
shalitha <mdsrwijethilaka@gmail.com>
2019-03-07 16:32:50 +0530

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,7 +6,7 @@ int num = 3;
 
 target =- num;  // Noncompliant; target = -3. Is that really what's meant?
 target =+ num; // Noncompliant; target = 3
-        System.out.println("simp            tg");
+        System.out.println("simp                                  tg");
         System.out.println("taaa  ssss  aaadffg");
     }
 }
