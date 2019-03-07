d41f8840d7da2d1c034df169844682abcbc5599d
shalitha <mdsrwijethilaka@gmail.com>
2019-03-07 17:14:55 +0530

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,6 +8,21 @@ target =- num;  // Noncompliant; target = -3. Is that really what's meant?
 target =+ num; // Noncompliant; target = 3
         System.out.println("simp            tg");
         System.out.println("taaa  ssss  aaadffg");
+        
+                try {
+            URL mySite = new URL("http://www.cs.utexas.edu/~scottm");
+            URLConnection yc = mySite.openConnection();
+            Scanner in = new Scanner(new InputStreamReader(yc.getInputStream()));
+            int count = 0;
+            while (in.hasNext()) {
+                System.out.println(in.next());
+                count++;
+            }
+            System.out.println("Number of tokens: " + count);
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
