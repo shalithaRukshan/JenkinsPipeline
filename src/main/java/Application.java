9a4018ff42be9e6f642d8ec9052c971b6cf5ecce
shalitha <mdsrwijethilaka@gmail.com>
2019-03-07 17:17:02 +0530

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -23,6 +23,37 @@ target =+ num; // Noncompliant; target = 3
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
+                int[] freqs = createFreqTableURL("http://www.utexas.edu/");
+
+        if( freqs.length == 0)
+            System.out.println("No frequency table created due to problems when reading from file");
+        else{
+            for(int i = 0; i < NUM_ASCII_CHAR; i++){
+                System.out.println("charcater code: " + i + " ,character: " + (char)i + " ,frequency: " + freqs[i]);
+            }
+            System.out.println("Total characters in file: " + sum(freqs));
+        }
+
+
+        freqs = new int[]{};
+        try{
+            freqs = createTable("ciaFactBook2008.txt");
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found. Unable to create freq table" + e);
+        }
+        catch(IOException e){
+            System.out.println("Problem while reading from file. Unable to create freq table" + e);
+        }
+        if( freqs.length == 0)
+            System.out.println("No frequency table created due to problems when reading from file");
+        else{
+            for(int i = 0; i < freqs.length; i++){
+                System.out.println("charcater code: " + i + " ,character: " + (char)i + " ,frequency: " + freqs[i]);
+            }
+            System.out.println("Total characters in file: " + sum(freqs));
+        }
     }
 }
 
