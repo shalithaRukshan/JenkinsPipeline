29f90ff95f3879a2d51eedca46ebcbb16dd90bb2
shalitha <mdsrwijethilaka@gmail.com>
2019-03-07 16:17:01 +0530

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,11 @@
 public class Application {
 
     public static void main(String[] args){
+        int target = -5;
+int num = 3;
+
+target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+target =+ num; // Noncompliant; target = 3
         System.out.println("simp            tg");
         System.out.println("taaa  ssss  aaadffg");
     }
