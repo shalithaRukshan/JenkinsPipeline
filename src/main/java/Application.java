5b7e1926e6978e125e24e74ccebc2b1aeaf4fa1d
shalitha <mdsrwijethilaka@gmail.com>
2019-03-07 16:36:44 +0530

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,5 +8,11 @@ target =- num;  // Noncompliant; target = -3. Is that really what's meant?
 target =+ num; // Noncompliant; target = 3
         System.out.println("simp                                  tg");
         System.out.println("taaa  ssss  aaadffg");
+        
     }
+}
+
+public boolean equals(Object obj) {
+  MyClass mc = (MyClass)obj;  // Noncompliant
+  // ...
 }
